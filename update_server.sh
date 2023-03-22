2102d646a3f71053d90517a112e6815716b0cd9a
gardc <gard@cappelendigital.no>
2023-03-22 16:13:17 +0100

    wip

--- update_server.sh
+++ update_server.sh
@@ -1,12 +1,15 @@
 
 # Stop the Docker Compose if it's already running
-ssh root@64.226.100.80 "cd ./ai4users-sandbox && docker-compose down"
-
+ssh root@64.226.100.80 << EOF
+    "cd ai4users-sandbox/ && docker-compose down"
+EOF
 
 cd ..
 
 # Upload the local files to the server
-rsync -ravz --delete ./ai4-users-sandbox root@64.226.100.80:ai4users-sandbox
+rsync -ravz --delete ai4-users-sandbox root@64.226.100.80:ai4users-sandbox
 
 # Start the Docker Compose on the server
-ssh root@64.226.100.80 "cd ./ai4users-sandbox && docker-compose up -d"
+ssh root@64.226.100.80 << EOF
+    "cd ai4users-sandbox/ && docker-compose build && docker-compose up -d"
+EOF
